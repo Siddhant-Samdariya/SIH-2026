f566145ac8cd8ddb8e163c2761c26f2b50d8230a
Sankalpshrivastava15 <sankalpshrivastava04@gmail.com>
2026-09-02 20:38:53 +0530

    new changes

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from Backend.api.video import router as video_router
+from Backend.api.detection import router as detection_router
+
 
 app = FastAPI(
     title="SIH Transport AI",
     description="AI-powered Transport Monitoring System",
     version="1.0.0"
 )
-
+app.include_router(video_router)
+app.include_router(detection_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
